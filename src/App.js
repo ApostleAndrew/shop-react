d18442f5479b24aacb9cc136dfe43be978c6ec10
Andrew <andriysof@gmail.com>
2018-11-03 12:39:09 +0200

    Add component App

--- src/App.js
+++ src/App.js
@@ -1,58 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <Test/>
-          
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <p>Hello world</p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Test1/>
-          <Test2/>
-        
-        </header>
-      </div>
-    );
-  }
+
+const App = () => {
+	return (
+		<div className="App">
+			Text
+		</div>
+	)
 }
 
-function Test() {
-  return (
-  <div>
-    <h1> Test component </h1>
-    <p> dd </p>
-  </div>
-  )
-}
 
-const Test1 = () => 
-  <div>
-    <h1> Test component </h1>
-    <p> dddddddddd </p>
-  </div>
+export default App
 
-const Test2 = () => {
-  return(
-    <div>
-      <h1> Test component3 </h1>
-      <p> dddd </p>
-    </div>
-  )
-}
 
-export default App;
+
