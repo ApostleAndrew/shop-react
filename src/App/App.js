9492b4530e33428add474fd7148b9c22d41a8b93
Andrew <andriysof@gmail.com>
2018-11-03 14:35:38 +0200

    Add separate folders Cart, Logo, Menu and add new folder common/style

--- src/App/App.js
+++ src/App/App.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 
 import Header from './Header/Header'
-import './menu.css'
-import './cart.css'
-import './base.css'
+
+
+import '../common/style/base.css'
+import '../common/style/reset.css'
 
 const App = () => {
 	return (
---
+++ src/App/Header/Logo/Logo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+
+const Logo = () => {
+	return(	
+	    <div className="logo">
+		    <img src="img/logo.png" alt=""/>
+	    </div>
+	)
+}
+
+
+export default Logo
