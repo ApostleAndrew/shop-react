e3ab41cf4f282671cb220864bbc5fe77c8e8aa71
Andrew <andriysof@gmail.com>
2018-11-10 12:47:29 +0200

    change props to desc

--- src/App/Main/Products/ProductsListItem.js
+++ src/App/Main/Products/ProductsListItem.js
@@ -2,19 +2,25 @@ import React from 'react'
 
 import './ProductsListItem.css'
 
-const ProductsListItem = (props) => {
+const ProductsListItem = ({
+    name,
+    deccription,
+    type,
+    capacity,
+    price,
+}) => {
     return (
         <div className='products-list-item'>
-            <div className='product-name'> {props.name}</div>
-            <div className='product-description'> {props.deccription}</div>
-            <div className='product-type'>Type: {props.type}</div>
-            <div className='product-capacity'> {props.capacity} Gb</div>
+            <div className='product-name'> {name}</div>
+            <div className='product-description'> {deccription}</div>
+            <div className='product-type'>Type: {type}</div>
+            <div className='product-capacity'> {capacity} Gb</div>
             <div className='poduct-quantity'>
                 <button>-</button>
                 <input type='text'/>
                 <button>+</button>
             </div>
-            <div className='product-price'> $ {props.price}</div>
+            <div className='product-price'> $ {price}</div>
             <button className='btn-add-to-cart'>Add to cart</button>
         </div>
     )
