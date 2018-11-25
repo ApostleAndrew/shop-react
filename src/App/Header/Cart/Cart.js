7ee3ed0f504d1cddb351e0b0233f84f2af6ad47a
Andrew <andriysof@gmail.com>
2018-11-25 14:03:35 +0200

    add key prouductID

--- src/App/Header/Cart/Cart.js
+++ src/App/Header/Cart/Cart.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import {keys} from 'lodash'
 import products from './../../Main/Products/products'
-
 import './cart.css'
 
+
+console.log(products)
 const Cart = ({
 	productsInCart,
 }) => {
@@ -12,12 +13,10 @@ const Cart = ({
         <div className="cart text-center">
 			{
 				keys(productsInCart).map((productId)=>(
-					<div>
+					<div key={productId}>
 						<span>{productId}</span>: {productsInCart[productId]}
 					</div>
 				))
-			// <div className="products-count"></div>
-			// <div className="products-price">$</div>
 			}
 		</div>
 	)
