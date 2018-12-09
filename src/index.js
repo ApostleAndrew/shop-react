5a6ecfadaed2ef45925b102c96d59d45d68608dc
Andrew <andrisof@gmail.com>
2018-12-09 14:08:39 +0200

    import Provider and add in app

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux'
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,document.getElementById('root'));
+    <Provider>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,document.getElementById('root'));
 
 
 // If you want your app to work offline and load faster, you can change
