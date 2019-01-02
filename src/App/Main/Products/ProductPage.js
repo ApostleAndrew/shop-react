8cee9fd525515a2b54fbd8c35abd085b418aae68
Andrew <andriysof@gmail.com>
2019-01-02 18:26:58 +0200

    c

--- src/App/Main/Products/ProductPage.js
+++ src/App/Main/Products/ProductPage.js
@@ -2,11 +2,13 @@ import React from 'react'
 import products, {getProductsMap} from './products'
 const ProductPage = ({
     productsItem = getProductsMap(products),
-    match
+    match,
 }) => {
     return(
         <div className = 'products-list'>
             <h2>{productsItem[match.params.productId].name}</h2>
+            <img src={productsItem[match.path.productId].image} alt={products.name}/>
+
         </div>
     )
 }
